f48726b652b5ea4125e80f1b0114ef38ace4796c
floyd10 <>
2018-12-12 21:33:58 +0500

    add main

---
+++ main.py
@@ -0,0 +1,14 @@
+import bitly_api
+
+
+BITLY_ACCESS_TOKEN = 'e3f866bffb9af54132642b96a67353cc4f6fc123'
+#e3f866bffb9af54132642b96a67353cc4f6fc123
+
+bitly = bitly_api.bitly_api.Connection(access_token=BITLY_ACCESS_TOKEN)
+longurl = 'http://mail.ru/'
+response = bitly.shorten(uri=longurl)
+shortUrl = response['url']
+cliks = bitly.link_clicks(link=shortUrl)
+#response = input("Enter a url: ")
+print (response['url'])
+print([cliks])
