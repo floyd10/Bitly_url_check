85551269a1009c70a0638bda856446e3f1fc52b6
floyd <none>
2018-12-13 08:17:31 +0500

    add country check

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import bitly_api
 
-
 BITLY_ACCESS_TOKEN = 'e3f866bffb9af54132642b96a67353cc4f6fc123'
 #e3f866bffb9af54132642b96a67353cc4f6fc123
 
@@ -10,5 +9,11 @@ response = bitly.shorten(uri=longurl)
 shortUrl = response['url']
 cliks = bitly.link_clicks(link=shortUrl)
 #response = input("Enter a url: ")
+
+
 print (response['url'])
+
+
+
 print([cliks])
+print((bitly.link_countries(link=shortUrl)))
