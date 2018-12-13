7c6201310734101c827c950d53ce39200d4a04ae
floyd <none>
2018-12-13 16:36:09 +0500

    fixes

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import requests
 import json
 import argparse
 
-
-url = 'http://mail.ru'
 BITLY_ACCESS_TOKEN = 'e3f866bffb9af54132642b96a67353cc4f6fc123'
 
 def url_shortner(url, BITLY_ACCESS_TOKEN):
@@ -33,20 +31,21 @@ def url_stat(url_short,BITLY_ACCESS_TOKEN):
     data = json.loads(response)['data']
     #print(data)
     clicks_data = (data.get('clicks')[0])
-    print('Информация по сокращенной ссылке :' , clicks_data)
+    #print('Информация по сокращенной ссылке :' , clicks_data)
     user_clicks = clicks_data['user_clicks']
     print('Количество переходов по сокращенной ссылке: ', user_clicks)
     return user_clicks
 
 
-def createParser():
+def createparser():
     parser = argparse.ArgumentParser()
     parser.add_argument('-l')
     return parser
 
 
-if createParser().parse_args().__contains__('l'):
- print ('Все ок!')
+if createparser().parse_args().__contains__('l'):
+ url = createparser().parse_args().l
+ print('Все ок!')
  try:
      if not url.startswith('http'):
          print(url, ' - кривая ссылка')
